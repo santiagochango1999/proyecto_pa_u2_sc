8eadd4b66ea1c783c9ce5e06b3a3985455b1a34d
Santiago Chango <santiojo@hotmail.com>
2023-01-06 19:44:18 -0500

    taller18

---
+++ src/main/java/com/example/demo/uce/repository/EstudianteRepoImp.java
@@ -0,0 +1,41 @@
+package com.example.demo.uce.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.uce.modelo.Estudiante;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class EstudianteRepoImp implements IEstudianteRepo{
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public void insertar(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+		this.entityManager.persist(estudiante);
+	}
+
+	@Override
+	public void actualizar(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+		this.entityManager.merge(estudiante);
+	}
+
+	@Override
+	public Estudiante buscar(Integer id) {
+		// TODO Auto-generated method stub
+		return this.entityManager.find(Estudiante.class, id);
+	}
+
+	@Override
+	public void eliminar(Integer id) {
+		// TODO Auto-generated method stub
+		Estudiante estu=this.buscar(id);
+		this.entityManager.remove(estu);
+	}
+
+}
---
+++ src/main/java/com/example/demo/uce/repository/IEstudianteRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.uce.repository;
+
+import com.example.demo.uce.modelo.Estudiante;
+
+public interface IEstudianteRepo {
+
+	public void insertar(Estudiante estudiante);
+
+	public void actualizar(Estudiante estudiante);
+
+	public Estudiante buscar(Integer id);
+
+	public void eliminar(Integer id);
+}
